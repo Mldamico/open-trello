d18ff87980b682b6f9fa568a7559426b08056b2d
mldamico <matias.lioneldamico@gmail.com>
2022-11-19 19:12:53 -0300

    Grid columns in home

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,18 @@ import { Layout } from "../components/layouts";
 
 export default function Home() {
   return (
-    <Layout>
-      <h1>Hola mundo</h1>
+    <Layout title="Home - Trello">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-x-2">
+        <div className="bg-slate-800 p-2 h-[calc(100vh_-_100px)]">
+          <h2>To Do</h2>
+        </div>
+        <div className="bg-slate-800 p-2 h-[calc(100vh_-_100px)]">
+          <h2>In Progress</h2>
+        </div>
+        <div className="bg-slate-800 p-2 h-[calc(100vh_-_100px)]">
+          <h2>Completed</h2>
+        </div>
+      </div>
     </Layout>
   );
 }
