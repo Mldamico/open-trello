603351a6f4c0cb6be2d615b411c93e51ed1a068c
mldamico <matias.lioneldamico@gmail.com>
2022-11-19 09:31:04 -0300

    theme setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@emotion/react";
-import { createTheme, CssBaseline } from "@mui/material";
-
-const theme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
+import { CssBaseline } from "@mui/material";
+import { lightTheme } from "../themes";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={lightTheme}>
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
